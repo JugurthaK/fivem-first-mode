3dde0cb2773355684d55219de5ef745077b1b83a
Kabeche Jugurtha <jugurtha.kabeche@orange.com>
2020-08-01 09:13:00 +0200

    First commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "bodacious"
+game "gta5"
+
+author "JK"
+description "First mode for FiveM"
+version "1.0.0"
+
+resource_type 'gametype' { name = "my game type"}
+
+client_script 'mymode_client.lua'
